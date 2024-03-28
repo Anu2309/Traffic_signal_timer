15f7b503b99385d71efac0732d365bfec6c5d1ae
Anu2309 <103429959+Anu2309@users.noreply.github.com>
2024-03-28 10:42:54 +0530

    Update setup.py

--- Code/YOLO/darkflow/setup.py
+++ Code/YOLO/darkflow/setup.py
@@ -3,72 +3,47 @@ from setuptools.extension import Extension
 from Cython.Build import cythonize
 import numpy
 import os
-import imp
 
-VERSION = imp.load_source('version', os.path.join('.', 'darkflow', 'version.py'))
+# Import the necessary modules directly without using 'imp' module
+import importlib.util
+spec = importlib.util.spec_from_file_location("version", os.path.join('.', 'darkflow', 'version.py'))
+VERSION = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(VERSION)
 VERSION = VERSION.__version__
 
-if os.name =='nt' :
-    ext_modules=[
-        Extension("darkflow.cython_utils.nms",
-            sources=["darkflow/cython_utils/nms.pyx"],
-            #libraries=["m"] # Unix-like specific
-            include_dirs=[numpy.get_include()]
-        ),        
-        Extension("darkflow.cython_utils.cy_yolo2_findboxes",
-            sources=["darkflow/cython_utils/cy_yolo2_findboxes.pyx"],
-            #libraries=["m"] # Unix-like specific
-            include_dirs=[numpy.get_include()]
-        ),
-        Extension("darkflow.cython_utils.cy_yolo_findboxes",
-            sources=["darkflow/cython_utils/cy_yolo_findboxes.pyx"],
-            #libraries=["m"] # Unix-like specific
-            include_dirs=[numpy.get_include()]
-        )
-    ]
+# Set Cython language level explicitly to avoid future warnings
+cython_directives = {"language_level": "3"}
 
-elif os.name =='posix' :
-    ext_modules=[
-        Extension("darkflow.cython_utils.nms",
-            sources=["darkflow/cython_utils/nms.pyx"],
-            libraries=["m"], # Unix-like specific
-            include_dirs=[numpy.get_include()]
-        ),        
-        Extension("darkflow.cython_utils.cy_yolo2_findboxes",
-            sources=["darkflow/cython_utils/cy_yolo2_findboxes.pyx"],
-            libraries=["m"], # Unix-like specific
-            include_dirs=[numpy.get_include()]
-        ),
-        Extension("darkflow.cython_utils.cy_yolo_findboxes",
-            sources=["darkflow/cython_utils/cy_yolo_findboxes.pyx"],
-            libraries=["m"], # Unix-like specific
-            include_dirs=[numpy.get_include()]
-        )
-    ]
+# Define the extensions
+ext_modules=[
+    Extension("darkflow.cython_utils.nms",
+        sources=["darkflow/cython_utils/nms.pyx"],
+        libraries=["m"],  # Unix-like specific
+        include_dirs=[numpy.get_include()],
+        cython_directives=cython_directives
+    ),        
+    Extension("darkflow.cython_utils.cy_yolo2_findboxes",
+        sources=["darkflow/cython_utils/cy_yolo2_findboxes.pyx"],
+        libraries=["m"],  # Unix-like specific
+        include_dirs=[numpy.get_include()],
+        cython_directives=cython_directives
+    ),
+    Extension("darkflow.cython_utils.cy_yolo_findboxes",
+        sources=["darkflow/cython_utils/cy_yolo_findboxes.pyx"],
+        libraries=["m"],  # Unix-like specific
+        include_dirs=[numpy.get_include()],
+        cython_directives=cython_directives
+    )
+]
 
-else :
-    ext_modules=[
-        Extension("darkflow.cython_utils.nms",
-            sources=["darkflow/cython_utils/nms.pyx"],
-            libraries=["m"] # Unix-like specific
-        ),        
-        Extension("darkflow.cython_utils.cy_yolo2_findboxes",
-            sources=["darkflow/cython_utils/cy_yolo2_findboxes.pyx"],
-            libraries=["m"] # Unix-like specific
-        ),
-        Extension("darkflow.cython_utils.cy_yolo_findboxes",
-            sources=["darkflow/cython_utils/cy_yolo_findboxes.pyx"],
-            libraries=["m"] # Unix-like specific
-        )
-    ]
-
+# Setup function
 setup(
     version=VERSION,
-	name='darkflow',
+    name='darkflow',
     description='Darkflow',
     license='GPLv3',
     url='https://github.com/thtrieu/darkflow',
-    packages = find_packages(),
-	scripts = ['flow'],
-    ext_modules = cythonize(ext_modules)
+    packages=find_packages(),
+    scripts=['flow'],
+    ext_modules=cythonize(ext_modules, compiler_directives=cython_directives)
 )
